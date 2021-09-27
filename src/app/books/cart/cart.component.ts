a55938c083244276e9484cfb5eaa4546b3453456
spjumde <spjumde@mitaoe.ac.in>
2021-09-27 13:47:24 +0530

    cart popup updated

--- src/app/books/cart/cart.component.ts
+++ src/app/books/cart/cart.component.ts
@@ -10,7 +10,22 @@ export class CartComponent implements OnInit {
   constructor(private bookServiceService: BookServiceService) {}
 
   public carts = [];
+  public books = [];
+  public booksOwned = [];
   ngOnInit() {
-    this.bookServiceService.getCart().subscribe((data) => (this.carts = data));
+    this.bookServiceService.getCart().subscribe((data) => {
+      this.carts = data;
+      this.bookServiceService.getBooks().subscribe((data) => {
+        this.books = data;
+        this.books.forEach((book) => {
+          this.carts.forEach((cart) => {
+            if (book.bookId == cart.bookId && cart.userId == '1') {
+              this.booksOwned.push(book);
+            }
+          });
+        });
+        // console.log('this.books :>> ', this.books, this.carts, this.booksOwned);
+      });
+    });
   }
 }
